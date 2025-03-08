b005a3c1ad661a2f2192648d61af217dc4ef9209
dolelongan <dolelongan@gmail.com>
2025-03-08 21:25:36 +0700

    chores: refactor

--- app/api/v1/api_v1_router.py
+++ app/api/v1/api_v1_router.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter
 
 from .endpoints.post import llm, rag_system
 from .endpoints.get import minio_storage
-from .endpoints import audio_processing, nurse
+from .endpoints import nurse, process_audio
 
 api_router = APIRouter()
 
 api_router.include_router(llm.router, prefix="/test/llm", tags=["test-llm"])
 api_router.include_router(minio_storage.router, tags=["minio-storage"])
 api_router.include_router(nurse.router, prefix="/nurses", tags=["nurses"])
-api_router.include_router(audio_processing.router, tags=["audio-processing"])
+api_router.include_router(process_audio.router, tags=["audio-processing"])
 api_router.include_router(rag_system.router, tags=["rag-system"])
